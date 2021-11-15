56cdb2defabc4486f0ccf1a19e27225e800f62d6
Devin Kelly-Collins <dkellycollins@dimin.com>
2021-11-15 10:12:43 -0600

    Made all options optional.

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,14 @@ import { ExtensionReference } from 'electron-devtools-installer';
 
 declare module '@dimensional-innovations/vue-electron-background' {
   export interface InitOptions {
-    config: any;
-    enableTouchEvents: boolean;
-    enableAutoUpdater: boolean;
-    enableKioskMode: boolean;
-    registerSchemesAsPrivileged: boolean;
-    browserWindowOptionOverrides: Partial<BrowserViewConstructorOptions>;
-    devTools: Array<string | ExtensionReference>;
+    config?: any;
+    enableTouchEvents?: boolean;
+    enableAutoUpdater?: boolean;
+    enableKioskMode?: boolean;
+    registerSchemesAsPrivileged?: boolean;
+    browserWindowOptionOverrides?: Partial<BrowserViewConstructorOptions>;
+    devTools?: Array<string | ExtensionReference>;
   }
 
-  export function init(options: InitOptions);
+  export function init(options: InitOptions): void;
 }
