8b2f560acb04923b3f288ecc82a566f26257fbcf
Devin Kelly-Collins <dkellycollins@dimin.com>
2025-01-08 11:56:49 -0600

    Updated electron-devtools-installer to the latest version to fix an issue with installing dev tools.

--- src/DevTools.ts
+++ src/DevTools.ts
@@ -1,8 +1,19 @@
 import { app } from 'electron';
-import install, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer';
+import { BACKBONE_DEBUGGER, EMBER_INSPECTOR, ExtensionReference, installExtension, JQUERY_DEBUGGER, MOBX_DEVTOOLS, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS, VUEJS_DEVTOOLS } from 'electron-devtools-installer';
 import { BrowserWindowInitContext, InitPlugin } from './init';
 
-export type Extension = string | typeof VUEJS3_DEVTOOLS;
+// Support backwards compatibility
+export type Extension = ExtensionReference;
+
+export const DevToolExtensions = {
+  EMBER_INSPECTOR,
+  REACT_DEVELOPER_TOOLS,
+  BACKBONE_DEBUGGER,
+  JQUERY_DEBUGGER,
+  VUEJS_DEVTOOLS,
+  REDUX_DEVTOOLS,
+  MOBX_DEVTOOLS,
+} as const;
 
 /**
  * Installs dev tools extensions and opens the devTools panel.
@@ -15,7 +26,7 @@ export class DevTools implements InitPlugin {
    * @param enabled - Indicates if the plugin is enabled. Used to disable the plugin when the app is packaged. Defaults to `!app.isPackaged`.
    */
   constructor(
-    private readonly devTools: Array<Extension>, 
+    private readonly devTools: Array<ExtensionReference>, 
     private readonly enabled: boolean = !app.isPackaged,
   ) { }
 
@@ -27,7 +38,7 @@ export class DevTools implements InitPlugin {
     browserWindow.on('ready-to-show', () => browserWindow.webContents.openDevTools());
 
     try {
-      await install(this.devTools);
+      await installExtension(this.devTools);
     } catch (error) {
       console.error(`Failed to install dev tools: ${this.devTools.join(',')}`);
       console.error(error);
